141d4f7a6e1bb0a6a55bd85d72f76f2786e7b54b
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-10-01 18:39:43 -0600

    Add endpoint for finding individual pallete

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pallete Picker';
 
 app.locals.projects = [
-  {id: 1, title: 'Project 1', palletes: [{name: 'pallete 1', id: 1, colors: ['#000000', '#FFFFFF', '#f1f1f1', '#f2f2f2', '#f5f5f5']}]}
+  {id: 1, title: 'Project 1', palletes: [{name: 'pallete 1', id: 2, colors: ['#000000', '#FFFFFF', '#f1f1f1', '#f2f2f2', '#f5f5f5']}]}
 ];
 
 app.get('/api/v1/projects', (request, response) => {
@@ -26,7 +26,17 @@ app.get('/api/v1/projects/:id', (request, response) => {
   }
 });
 
-
+app.get('/api/v1/projects/:projectid/palletes/:palleteid', (request, response) => {
+  const projectid = request.params.projectid;
+  const palleteid = request.params.palleteid;
+  const project = app.locals.projects.find(project => project.id == projectid);
+  const pallete = project.palletes.find(pallete => pallete.id == palleteid);
+  if (pallete) {
+    return response.status(200).json(pallete);
+  } else {
+    return response.status(404).json({error: 'Sorry that pallete does not exist'});
+  }
+})
 
 app.listen(3000, () => {
   console.log('Pallete Picker is running on port 3000');
