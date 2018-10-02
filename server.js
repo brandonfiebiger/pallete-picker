dbf4cff7cb4f6ddc6b2551dad1db5a5919014372
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-10-01 18:27:03 -0600

    Add endpoints for getting all projects and getting project by id

--- server.js
+++ server.js
@@ -5,12 +5,29 @@ const app = express();
 app.use(express.static('public'));
 
 app.set('port', process.env.PORT || 3000);
+
 app.locals.title = 'Pallete Picker';
 
-app.get('/', (request, response) => {
-  response.status(200).send('Hello World!');
+app.locals.projects = [
+  {id: 1, title: 'Project 1', palletes: [{name: 'pallete 1', id: 1, colors: ['#000000', '#FFFFFF', '#f1f1f1', '#f2f2f2', '#f5f5f5']}]}
+];
+
+app.get('/api/v1/projects', (request, response) => {
+  response.status(200).json(app.locals.projects);
 });
+
+app.get('/api/v1/projects/:id', (request, response) => {
+  const id = request.params.id;
+  const project = app.locals.projects.find(project => project.id == id);
+  if (project) {
+    return response.status(200).json(project);
+  } else {
+    return response.status(404).json({error: 'Sorry that project does not exist.'});
+  }
+});
+
+
 
 app.listen(3000, () => {
   console.log('Pallete Picker is running on port 3000');
-})
+});
