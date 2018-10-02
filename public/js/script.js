3000c67c785bbaabab3847d651e4ac349ac15820
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-10-02 16:02:49 -0600

    Create function to randomly generate hex codes

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+// const randomHexColor = require('random-hex-color')
+
 fetch('http://localhost:3000/api/v1/projects/new', {
   method: 'POST',
   body: JSON.stringify({
@@ -5,9 +7,22 @@ fetch('http://localhost:3000/api/v1/projects/new', {
     palletes: ['world']
   }),
   headers: {
-    'Content-Type': 'application/json',
-  },
+    'Content-Type': 'application/json'
+  }
 })
+.then(response => response.json())
 .then(data => console.log(data))
 .catch(error => console.log(error))
+
+// console.log(randomHexColor())
+
+const generateRandomHexCode = () => {
+  const possibleDigits = "0123456789ABCDEF"; 
+  let color = []
+  for (let i = 0; i < 6; i++) {
+    color.push(possibleDigits[Math.floor(Math.random() * 16)]);
+  }
+  color.join('')
+  return '#' + color.join('');
+}
 
